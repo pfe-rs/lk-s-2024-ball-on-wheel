0432f7999bfcdded2285a56e03af3019d8a774e7
Lenka Jakovljevic <lenkajakovljevic2@gmail.com>
2024-07-19 19:05:45 +0200

    detekcija loptice

--- detekcija plave loptice i koord klik.py
+++ detekcija plave loptice i koord klik.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 
 
-koordinatni_pocetak = None
+koordinatni_pocetak = (0,0)
 
 #da se koordinatni pocetak postavi tamo gde se klikne (treba biti centar tocka)
 def click(event, x, y, flags, param):
@@ -16,30 +16,25 @@ cap = cv2.VideoCapture(0)
 cv2.namedWindow('Frame')
 cv2.setMouseCallback('Frame', click)
 
-#za pracenje loptice
-kalman = cv2.KalmanFilter(4, 2)
-kalman.measurementMatrix = np.array([[1, 0, 0, 0],
-                                     [0, 1, 0, 0]], np.float32)
-kalman.transitionMatrix = np.array([[1, 0, 1, 0],
-                                    [0, 1, 0, 1],
-                                    [0, 0, 1, 0],
-                                    [0, 0, 0, 1]], np.float32)
-kalman.processNoiseCov = np.eye(4, dtype=np.float32) * 0.03
-
-
+roi_x, roi_y, roi_w, roi_h = 100, 100, 200, 300
 
 while True:
     ret, frame = cap.read()
     
     if not ret:
         break
+
+      
+    # Isjecanje ROI iz frejma
+    roi = frame[roi_y:roi_y+roi_h, roi_x:roi_x+roi_w]
     
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_plava = np.array([100, 150, 50])
-    upper_plava = np.array([140, 255, 255])
+    lower_plava = np.array([0, 50, 30])
+    upper_plava = np.array([25, 185, 225])
 
     mask = cv2.inRange(hsv, lower_plava, upper_plava)
+
     blue_only = cv2.bitwise_and(frame, frame, mask=mask)
     gray = cv2.cvtColor(blue_only, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (9, 9), 2)
@@ -59,20 +54,11 @@ while True:
             angle = math.degrees(math.atan2(new_y, new_x))
             
             cv2.putText(frame, f"Angle: {angle:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-            
-            merenje = np.array([[np.float32(x)], [np.float32(y)]])
-            kalman.correct(merenje)
-            prediction = kalman.predict()
-            
-            pred_x, pred_y = prediction[0], prediction[1]
 
-            vel_x, vel_y = prediction[2], prediction[3]
-
-            print(vel_x, vel_y)
-        
         cv2.line(frame, (koordinatni_pocetak_x, 0), (koordinatni_pocetak_x, frame.shape[0]), (0, 0, 255), 2)
         cv2.line(frame, (0, koordinatni_pocetak_y), (frame.shape[1], koordinatni_pocetak_y), (0, 255, 0), 2)
 
+    frame[roi_y:roi_y+roi_h, roi_x:roi_x+roi_w] = roi
     cv2.imshow('Frame', frame)
     
     key = cv2.waitKey(1) & 0xFF
