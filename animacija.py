c8c3f4a7172c236ad9e4b216341e0fda45620fb3
Lenka Jakovljevic <lenkajakovljevic2@gmail.com>
2024-07-20 13:46:44 +0200

    popravljen kod

--- animacija.py
+++ animacija.py
@@ -1,37 +1,38 @@
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
 
-def animacija(r_tocak, R_loptica):
-               fig, ax = plt.subplots()
+def animacija(r_tocak, R_loptica, t, x1, x3):
+    fig, ax = plt.subplots()
 
-               circle_tocak = plt.Circle((0, 0), r_tocak, color='blue', fill=False)
-               ax.add_artist(circle_tocak)
+    circle_tocak = plt.Circle((0, 0), r_tocak, color='blue', fill=False)
+    ax.add_artist(circle_tocak)
 
-               circle_small = plt.Circle((0, 0), R_loptica, color='red', fill=False)
-               ax.add_artist(circle_small)
+    circle_small = plt.Circle((0, 0), R_loptica, color='red', fill=False)
+    ax.add_artist(circle_small)
 
-               dot, = ax.plot([], [], 'ko', markersize=5)
+    dot, = ax.plot([], [], 'ko', markersize=5)
 
-               # Postavi limite i proporcije
-               ax.set_xlim(-r_tocak - R_loptica - 1, r_tocak + R_loptica + 1)
-               ax.set_ylim(-r_tocak - R_loptica - 1, r_tocak + R_loptica + 1)
-               ax.set_aspect('equal', 'box')
+    ax.set_xlim(-r_tocak - R_loptica - 1, r_tocak + R_loptica + 1)
+    ax.set_ylim(-r_tocak - R_loptica - 1, r_tocak + R_loptica + 1)
+    ax.set_aspect('equal', 'box')
 
-               def update(frame, x1):
-                              # Ažuriraj koordinate manjeg kruga
-                              x1_current = x1[frame]
-                              x_small = (r_tocak + R_loptica) * np.sin(x1_current)
-                              y_small = (r_tocak + R_loptica) * np.cos(x1_current)
-                              circle_small.center = (x_small, y_small)
-                              
-                              # koordinate crne tačkice, da se pokaze rotacija tocka
-                              x3_current = x3[frame]
-                              x_dot = r_tocak * np.sin(x3_current)
-                              y_dot = r_tocak * np.cos(x3_current)
-                              dot.set_data(x_dot, y_dot)
-                              
-                              return circle_small, dot
+    def update(frame):
+       
+        x1_current = x1[frame]
+        x_small = (r_tocak + R_loptica) * np.sin(x1_current)
+        y_small = (r_tocak + R_loptica) * np.cos(x1_current)
+        circle_small.center = (x_small, y_small)
 
-                              ani = animation.FuncAnimation(fig, update, frames=len(t), interval=1000/30, blit=True)
+        # koordinate crne tačkice, da se pokaze rotacija tocka
+        x3_current = x3[frame]
+        x_dot = r_tocak * np.sin(x3_current)
+        y_dot = r_tocak * np.cos(x3_current)
+        dot.set_data(x_dot, y_dot)
 
-                              plt.grid(True)
-                              plt.show()
+        return circle_small, dot
+
+    ani = animation.FuncAnimation(fig, update, frames=len(t), interval=1000/30, blit=True)
+
+    plt.grid(True)
+    plt.show()
--- simlacija.py
+++ simlacija.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from scipy.integrate import solve_ivp
+import animacija
 
 # Konstante 
 Km = 0.1
@@ -55,40 +56,4 @@ x3 = solution.y[2]
 r_tocak = 3
 R_loptica = 1
 
-fig, ax = plt.subplots()
-
-circle_tocak = plt.Circle((0, 0), r_tocak, color='blue', fill=False)
-ax.add_artist(circle_tocak)
-
-circle_small = plt.Circle((0, 0), R_loptica, color='red', fill=False)
-ax.add_artist(circle_small)
-
-dot, = ax.plot([], [], 'ko', markersize=5)
-
-# Postavi limite i proporcije
-ax.set_xlim(-r_tocak - R_loptica - 1, r_tocak + R_loptica + 1)
-ax.set_ylim(-r_tocak - R_loptica - 1, r_tocak + R_loptica + 1)
-ax.set_aspect('equal', 'box')
-
-def update(frame):
-    # Ažuriraj koordinate manjeg kruga
-    x1_current = x1[frame]
-    x_small = (r_tocak + R_loptica) * np.sin(x1_current)
-    y_small = (r_tocak + R_loptica) * np.cos(x1_current)
-    circle_small.center = (x_small, y_small)
-
-    # koordinate crne tačkice, da se pokaze rotacija tocka
-    x3_current = x3[frame]
-    x_dot = r_tocak * np.sin(x3_current)
-    y_dot = r_tocak * np.cos(x3_current)
-    dot.set_data(x_dot, y_dot)
-
-    return circle_small, dot
-
-
-ani = animation.FuncAnimation(fig, update, frames=len(t), interval=1000/30, blit=True)
-
-
-plt.grid(True)
-plt.show()
-
+animacija.animacija(r_tocak, R_loptica, t, x1, x3)
