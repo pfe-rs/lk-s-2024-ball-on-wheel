f9da97b2dd9a6c07c574687377e3b7561ec6e355
Lenka Jakovljevic <lenkajakovljevic2@gmail.com>
2024-07-22 22:50:13 +0200

    napravljeno kao klasa

--- balldetection.py
+++ balldetection.py
@@ -2,63 +2,69 @@ import cv2
 import numpy as np
 import math
 
-koordinatni_pocetak = (0, 0)
 
-def click(event, x, y, flags, param):
-    global koordinatni_pocetak
-    if event == cv2.EVENT_LBUTTONDOWN:
-        koordinatni_pocetak = (x, y)
+class detekcija_loptice():
+    def __init__ (self):
+        self.koordinatni_pocetak = (0, 0)
+        self.roi_x, self.roi_y, self.roi_w, self.roi_h = 100, 100, 200, 300
 
-def get_polozaj():
-    global koordinatni_pocetak, roi_x, roi_y, roi_h, roi_w, frame
-    roi = frame[roi_y:roi_y+roi_h, roi_x:roi_x+roi_w]
+    def click(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.koordinatni_pocetak = (x, y)
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    def get_polozaj(self, frame):
+        roi = frame[roi_y:roi_y+roi_h, roi_x:roi_x+roi_w]
 
-    lower_plava = np.array([0, 50, 30])
-    upper_plava = np.array([25, 185, 225])
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    mask = cv2.inRange(hsv, lower_plava, upper_plava)
+        lower_plava = np.array([0, 50, 30])
+        upper_plava = np.array([25, 185, 225])
 
-    blue_only = cv2.bitwise_and(frame, frame, mask=mask)
-    gray = cv2.cvtColor(blue_only, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (9, 9), 2)
-    
-    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1.2, minDist=100, param1=100, param2=30, minRadius=10, maxRadius=100)
+        mask = cv2.inRange(hsv, lower_plava, upper_plava)
 
-    if circles is not None:
-        circles = np.round(circles[0, :]).astype("int")
-        for (x, y, r) in circles:
-            if koordinatni_pocetak is not None:
-                koordinatni_pocetak_x, koordinatni_pocetak_y = koordinatni_pocetak
+        blue_only = cv2.bitwise_and(frame, frame, mask=mask)
+        gray = cv2.cvtColor(blue_only, cv2.COLOR_BGR2GRAY)
+        blurred = cv2.GaussianBlur(gray, (9, 9), 2)
+        
+        circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1.2, minDist=100, param1=100, param2=30, minRadius=10, maxRadius=100)
 
-                new_x = x - koordinatni_pocetak_x
-                new_y = koordinatni_pocetak_y - y
-                angle = math.degrees(math.atan2(new_y, new_x))
-                
-                return angle
+        if circles is not None:
+            circles = np.round(circles[0, :]).astype("int")
+            for (x, y, r) in circles:
+                if self.koordinatni_pocetak is not None:
+                    koordinatni_pocetak_x, koordinatni_pocetak_y = self.koordinatni_pocetak
 
-cap = cv2.VideoCapture(0)
-cv2.namedWindow('Frame')
-cv2.setMouseCallback('Frame', click)
-
-roi_x, roi_y, roi_w, roi_h = 100, 100, 200, 300
+                    new_x = x - koordinatni_pocetak_x
+                    new_y = koordinatni_pocetak_y - y
+                    angle = math.degrees(math.atan2(new_y, new_x))
+                    
+                    return angle
 
 
-while True:
-    ret, frame = cap.read()
+if __name__ == "__main__":
 
-    if not ret:
-        break
+    bd = detekcija_loptice()
+    cap = cv2.VideoCapture(0)
+    cv2.namedWindow('Frame')
+    cv2.setMouseCallback('Frame', bd.click)
 
-    angle = get_polozaj()
-    print(angle)
+    roi_x, roi_y, roi_w, roi_h = 100, 100, 200, 300
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
 
-cap.release()
-cv2.destroyAllWindows()
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        angle = bd.get_polozaj(frame)
+        print(angle)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 #захвалност никлоли најићу!!
