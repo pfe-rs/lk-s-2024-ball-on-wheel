9aedf1734b99ac79b2984a9a998212079de1f380
nemanja <obradovicn133@gmail.com>
2024-07-23 22:21:23 +0200

    Motor_arduino_python.v3

--- motor.py
+++ motor.py
@@ -4,17 +4,18 @@ import time
 arduino = serial.Serial('/dev/ttyACM1',115200)
 
 class Motor:
-    def salji_napon(self,napon,arduino):
+    @staticmethod
+    def salji_napon(napon,arduino):
         if 0 <= napon <= 255:
             arduino.write(f"{napon}\n".encode())
             time.sleep(0.1)  # Allow some time for the Arduino to process
             while arduino.in_waiting > 0:
                 arduino.readline()  # Clear the buffer
 
-time.sleep(1)
+"""time.sleep(1)
 
 while True:    
     print("Uneti napon")
     napon=int(input())
     motor=Motor()
-    motor.salji_napon(napon,arduino)
+    motor.salji_napon(napon,arduino) """
