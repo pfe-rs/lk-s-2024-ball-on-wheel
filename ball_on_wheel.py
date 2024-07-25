b688a262f56509245f8e010590ab4f4346f61fce
nemanja <obradovicn133@gmail.com>
2024-07-25 15:44:37 +0200

    Povratna_sprega_za_brzinu_koja_zapravo_radi.v1

--- ball_on_wheel.py
+++ ball_on_wheel.py
@@ -8,8 +8,8 @@ import matplotlib
 from matplotlib import pyplot as plt
 matplotlib.use('TkAgg')
 
-zeljena_ugaona_brzina=200
-zeljena_brzina2 = 350 
+zeljena_ugaona_brzina=2000
+zeljena_brzina2 = 2500
 
 
 duzina = 30
@@ -27,8 +27,11 @@ enkoder=enkoder_motor.Enkoder()
 motora=motor.Motor()
 interval=0.01           #vremenski interval posle kog ce se izvrsiti fja
 prethodno_vreme=time.time()
+arduino.write(b'start\n')
 
 while time.time() < start_time + duzina:
+    s = time.time()
+    
     trenutno_vreme=time.time()
     if(trenutno_vreme-prethodno_vreme>=interval):
         prethodno_vreme=trenutno_vreme
@@ -40,15 +43,17 @@ while time.time() < start_time + duzina:
             greska=PID.Geterror(zeljena_ugaona_brzina,ugaona_brzina)
             napon=PID.pid(greska,0)
             motora.salji_napon(napon,arduino) 
-
             sad_time = time.time() - start_time
             brzina_list.append(ugaona_brzina)
             vreme_list.append(sad_time)
             napon_list.append(napon)
             zeljena_ugaona_brzina_list.append(zeljena_ugaona_brzina)
+    print(time.time()-s)
 
-
-
+plt.title("zavisnost brzine i napona od vremena")
+plt.xlabel("vreme")
+plt.ylabel("")
+plt.legend(loc='upper left', fontsize='large', title='Legend', frameon=False)
 plt.plot(vreme_list,brzina_list)
 plt.plot(vreme_list,napon_list)
 plt.plot(vreme_list, zeljena_ugaona_brzina_list)
