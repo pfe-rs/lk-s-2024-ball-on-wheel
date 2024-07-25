3b772aafadc6b489b4e80b033a7b0d871c054c2c
nemanja <obradovicn133@gmail.com>
2024-07-25 11:44:24 +0200

    Sredjena_sinteza

--- ball_on_wheel.py
+++ ball_on_wheel.py
@@ -8,12 +8,12 @@ import matplotlib
 from matplotlib import pyplot as plt
 matplotlib.use('TkAgg')
 
-zeljena_ugaona_brzina=210
+zeljena_ugaona_brzina=200
 zeljena_brzina2 = 350 
 
 
-duzina = 40
-neko_vreme = 20
+duzina = 30
+neko_vreme = 15
 start_time = time.time()
 
 brzina_list = []
@@ -24,14 +24,14 @@ zeljena_ugaona_brzina_list = []
 arduino = serial.Serial('/dev/ttyACM0',115200)
 
 enkoder=enkoder_motor.Enkoder()
-motor=motor.Motor()
-#interval=0.01           #vremenski interval posle kog ce se izvrsiti fja
-#prethodno_vreme=time.time()
+motora=motor.Motor()
+interval=0.01           #vremenski interval posle kog ce se izvrsiti fja
+prethodno_vreme=time.time()
 
 while time.time() < start_time + duzina:
-    #trenutno_vreme=time.time()
-    #if(trenutno_vreme-prethodno_vreme>=interval):
-      #  prethodno_vreme=trenutno_vreme
+    trenutno_vreme=time.time()
+    if(trenutno_vreme-prethodno_vreme>=interval):
+        prethodno_vreme=trenutno_vreme
         enkoder.citaj_podatke(arduino)
         ugaona_brzina=enkoder.odredjivanje_ugaone_brzine()
         if ugaona_brzina is not None:
@@ -39,7 +39,7 @@ while time.time() < start_time + duzina:
                 zeljena_ugaona_brzina = zeljena_brzina2
             greska=PID.Geterror(zeljena_ugaona_brzina,ugaona_brzina)
             napon=PID.pid(greska,0)
-            motor.salji_napon(napon,arduino)  
+            motora.salji_napon(napon,arduino) 
 
             sad_time = time.time() - start_time
             brzina_list.append(ugaona_brzina)
