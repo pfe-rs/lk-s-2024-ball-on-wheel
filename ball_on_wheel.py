cd97f2f197780f06cb0b89f55521514435a32173
nemanja <obradovicn133@gmail.com>
2024-07-26 10:59:01 +0200

    menjano

--- ball_on_wheel.py
+++ ball_on_wheel.py
@@ -3,7 +3,7 @@ import serial
 import enkoder_motor
 import motor
 import PID
-import balldetection
+import sakalmanovim
 import time 
 import matplotlib
 
@@ -14,7 +14,7 @@ zeljena_ugaona_brzina=2000
 zeljena_brzina2 = 2500
 
 
-duzina = 10
+duzina = 5
 neko_vreme = 5
 start_time = time.time()
 
@@ -22,44 +22,55 @@ brzina_list = []
 vreme_list = []
 napon_list= []
 zeljena_ugaona_brzina_list = []
+anglelist= []
 
 arduino = serial.Serial('/dev/ttyACM0',115200)
 
 enkoder=enkoder_motor.Enkoder()
 motora=motor.Motor()
-loptica=balldetection.detekcija_loptice()
+loptica=sakalmanovim.detekcija_loptice()
 cap=cv2.VideoCapture(0)
-cv2.namedWindow('Frame')
-cv2.setMouseCallback('Frame', loptica.click)
-roi_x, roi_y, roi_w, roi_h = 100, 100, 200, 300
-
-
+#cv2.namedWindow('Frame')
+#cv2.setMouseCallback('Frame', loptica.click)
+#roi_x, roi_y, roi_w, roi_h = 100, 100, 200, 300
 interval=0.01           #vremenski interval posle kog ce se izvrsiti fje
 prethodno_vreme=time.time()
 arduino.write(b'start\n')
 
 while time.time() < start_time + duzina:
+    s=time.time
     trenutno_vreme=time.time()
     if(trenutno_vreme-prethodno_vreme>=interval):
         prethodno_vreme=trenutno_vreme
 
         ret, frame = cap.read()
+        """if not ret:
+            break"""
         angle, brzina_loptice = loptica.get_polozaj(frame)
-        cv2.imshow('Frame', frame)
+        """cv2.imshow('Frame', frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break"""
+        
+        #angle controll
 
         enkoder.citaj_podatke(arduino)
         ugaona_brzina=enkoder.odredjivanje_ugaone_brzine()
-        if ugaona_brzina is not None:
-            if time.time() > start_time + neko_vreme:
-                zeljena_ugaona_brzina = zeljena_brzina2
-            greska=PID.Geterror(zeljena_ugaona_brzina,ugaona_brzina)
-            napon=PID.pid(greska,0)
-            motora.salji_napon(napon,arduino) 
-            sad_time = time.time() - start_time
-            brzina_list.append(ugaona_brzina)
-            vreme_list.append(sad_time)
-            napon_list.append(napon)
-            zeljena_ugaona_brzina_list.append(zeljena_ugaona_brzina)
+        if ugaona_brzina is None:
+            ugaona_brzina = 0
+        # if ugaona_brzina is not None:
+        if time.time() > start_time + neko_vreme:
+            zeljena_ugaona_brzina = zeljena_brzina2
+        greska=PID.Geterror(zeljena_ugaona_brzina,ugaona_brzina)
+        napon=PID.pid(greska,0)
+        motora.salji_napon(napon,arduino) 
+        sad_time = time.time() - start_time
+        brzina_list.append(ugaona_brzina)
+        vreme_list.append(sad_time)
+        napon_list.append(napon)
+        zeljena_ugaona_brzina_list.append(zeljena_ugaona_brzina)
+        anglelist.append(angle)
+
 
 cap.release()
 cv2.destroyAllWindows()
@@ -67,8 +78,10 @@ cv2.destroyAllWindows()
 plt.title("zavisnost brzine i napona od vremena")
 plt.xlabel("vreme")
 plt.ylabel("")
-#plt.legend(loc='upper left', fontsize='large', title='Legend', frameon=False)
+# plt.legend(loc='upper left', fontsize='large', title='Legend', frameon=False)
 plt.plot(vreme_list,brzina_list)
 plt.plot(vreme_list,napon_list)
+plt.plot(vreme_list, anglelist, marker='o')
 plt.plot(vreme_list, zeljena_ugaona_brzina_list)
 plt.show() 
+a = 1
--- sakalmanovim.py
+++ sakalmanovim.py
@@ -3,7 +3,6 @@ import numpy as np
 import math
 import time
 from filterpy.kalman import KalmanFilter
-
 class detekcija_loptice():
     def __init__ (self):
         self.koordinatni_pocetak = (0, 0)
@@ -37,8 +36,9 @@ class detekcija_loptice():
         gray = cv2.cvtColor(blue_only, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray, (9, 9), 2)
         
+        s = time.time()
         circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1.2, minDist=100, param1=100, param2=30, minRadius=10, maxRadius=100)
-
+        print(time.time()-s)
         if circles is not None:
             circles = np.round(circles[0, :]).astype("int")
             for (x, y, r) in circles:
@@ -62,11 +62,11 @@ class detekcija_loptice():
 
                     self.pre_time = curr_time
                     
-                    cv2.circle(frame, (x, y), r, (0, 255, 0), 2)
-                    cv2.circle(frame, (x, y), 2, (0, 255, 0), 3)
+                    # cv2.circle(frame, (x, y), r, (0, 255, 0), 2)
+                    # cv2.circle(frame, (x, y), 2, (0, 255, 0), 3)
                     
-                    cv2.putText(frame, f"Angle: {angle:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-                    cv2.putText(frame, f"Angular Speed: {brzina_loptice:.2f}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+                    # cv2.putText(frame, f"Angle: {angle:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+                    # cv2.putText(frame, f"Angular Speed: {brzina_loptice:.2f}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
                    
                     return angle, brzina_loptice
         
